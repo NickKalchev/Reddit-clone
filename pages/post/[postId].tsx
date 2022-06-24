cd7091468cbf9344f14920cd2e4b68c156031387
Nikolay Kalchev <n.kalchev1711@gmail.com>
2022-06-24 22:01:05 +0200

    Created Top Communities

--- pages/post/[postId].tsx
+++ pages/post/[postId].tsx
@@ -72,6 +72,7 @@ function PostPage() {
                                 placeholder={session ? "What are your thoughts?" : "Please sign in to comment"}
                             />
                             <button 
+                                disabled={!session}
                                 type="submit" 
                                 className="rounded-full self-center bg-red-500 font-semibold text-white disabled:bg-gray-200 px-20 py-2"
                             >
